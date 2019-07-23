1bae82e35c87dde8427071a10855591687ad909a
Felipe Marin <felipe.marin@gmail.com>
2019-07-23 14:39:00 -0500

    Changed router js chunks

--- src/router.js
+++ src/router.js
@@ -17,27 +17,27 @@ export default new Router({
       path: '/shows',
       name: 'shows',
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (shows.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Shows.vue')
+      component: () => import(/* webpackChunkName: "shows" */ './views/Shows.vue')
     },
     {
       path: '/shows/:id/:name',
       name: 'show',
       props: true,
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (show.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Show.vue')
+      component: () => import(/* webpackChunkName: "show" */ './views/Show.vue')
     },
     {
       path: '/search',
       name: 'search',
       props: true,
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (search.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Search.vue')
+      component: () => import(/* webpackChunkName: "search" */ './views/Search.vue')
     }
   ]
 })
